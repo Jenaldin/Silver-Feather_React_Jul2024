0ea37affd675c6dfa5d5107d2c39436b7eb39349
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-07-30 00:38:00 +0300

    Created authMiddleware in server

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require('../utils/jwt');
+const secret = process.env.SECRET;
+
+exports.authMiddleware = async (req, res, next) => {
+  const token = req.cookies['auth'];
+
+  if (!token) {
+    return next();
+  };
+
+  try {
+    const decodedToken = await jwt.verify(token, secret);
+    req.user = decodedToken;
+    res.locals.isAuthenticated = true;
+    next();
+  } catch (err) {
+    res.clearCookie('auth');
+    res.status(401).json({ message: 'Unauthorized' });
+  };
+};
+
+exports.isAuth = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  };
+  next();
+};
+
+exports.isGuest = (req, res, next) => {
+  if (req.user) {
+    return res.status(403).json({ message: 'Forbidden' });
+  };
+  next();
+};
