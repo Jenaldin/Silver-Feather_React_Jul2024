ab1fc77a7764123ba1bc392c20ae73b34b465aa9
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-07-31 01:10:15 +0300

    Created Login component

---
+++ client/src/components/login/Login.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+
+import { Avatar, Button, CssBaseline, TextField, Grid, Box, Typography, Container } from "@mui/material";
+import LoginIcon from '@mui/icons-material/Login';
+
+import { theme } from '../../../public/styles/muiTheme';
+
+export default function Login() {
+   const { handleSubmit, register, formState: { errors }, trigger } = useForm();
+
+   const onSubmit = (data) => {
+      console.log(data);
+   };
+
+   return (
+      <Container component="main" maxWidth="xs">
+         <CssBaseline />
+         <Box sx={{ marginTop: 15, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+               <LoginIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+               Sign-in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 3 }}>
+               <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                     <TextField
+                        {...register('username', {
+                           required: 'Username is required',
+                           pattern: {
+                              value: /^[a-zA-Z0-9_-]+$/,
+                              message: 'Invalid username',
+                           },
+                           validate: value => !/\s/.test(value) || 'No spaces allowed',
+                        })}
+                        fullWidth
+                        name="username"
+                        id="username"
+                        label="Username"
+                        type="input"
+                        error={!!errors.username}
+                        helperText={errors.username?.message}
+                        onBlur={() => trigger('username')}
+                     />
+                  </Grid>
+                  <Grid item xs={12}>
+                     <TextField
+                        {...register('password', {
+                           required: 'Password is required',
+                           validate: value => !/\s/.test(value) || 'No spaces allowed',
+                        })}
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        error={!!errors.password}
+                        helperText={errors.password?.message}
+                        onBlur={() => trigger('password')}
+                     />
+                  </Grid>
+               </Grid>
+               <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+                  Sign-in
+               </Button>
+               <Grid container justifyContent="flex-end">
+                  <Grid item>
+                     <Link to="/register" style={{ textDecoration: 'none', color: theme.palette.primary.dark }}>
+                        Not a member of the Tavern? Register!
+                     </Link>
+                  </Grid>
+               </Grid>
+            </Box>
+         </Box>
+      </Container>
+   );
+}
