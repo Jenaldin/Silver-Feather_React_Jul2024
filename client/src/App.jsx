13609be3e8e3262e13afa83b1c63445082cb9628
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-07-31 01:10:34 +0300

    Edited routes for register and login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import { theme } from '../public/styles/muiTheme';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/sign-in' element={<Login />} />
         </Routes>
       </main>
       </div>
