bb69371d03764b6b9a3f63b3e29447302ab3dabc
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-07-30 19:59:06 +0300

    Added graphic elements and styling for better look

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -13,45 +13,45 @@ export default function Header() {
       margin: '0 10px',
       color: location.pathname === path ? theme.palette.primary.contrastText : theme.palette.primary.dark,
       '&:hover': {
-          backgroundColor: theme.palette.primary.light,
-          color: theme.palette.background.default,
+         backgroundColor: theme.palette.primary.light,
+         color: theme.palette.background.default,
       },
-  });
+   });
 
    return (
       <div>
-      <AppBar position="fixed" sx={{ top: 0, left: 0, right: 0 }}>
-         <Toolbar style={{ minHeight: '50px', paddingLeft: '0px', paddingRight: '0px' }}>
-            <Link to="/" style={{ padding: '0', margin: '0' }}>
-               <img src="/images/favicon.png" alt="Logo" 
-               style={{ height: '50px', width: '50px', marginRight: '16px', objectFit: 'cover' }} />
-            </Link>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-               <Link to="/" 
-               style={{ textDecoration: 'none', color: theme.palette.background.paper, fontWeight: 'bold', fontStyle: 'italic' }}>
-                  Silver Feather Tavern
+         <AppBar position="fixed" sx={{ top: 0, left: 0, right: 0 }}>
+            <Toolbar style={{ minHeight: '50px', paddingLeft: '0px', paddingRight: '0px' }}>
+               <Link to="/" style={{ padding: '0', margin: '0' }}>
+                  <img src="/images/favicon.png" alt="Logo"
+                     style={{ height: '50px', width: '50px', marginRight: '16px', objectFit: 'cover' }} />
                </Link>
-            </Typography>
-                        <Link to="/adventurers-board" style={{ textDecoration: 'none' }}>
-               <Button sx={buttonStyle('/adventurers-board')}>Adventurer's Board</Button>
-            </Link>
-            <Link to="/my-boards" style={{ textDecoration: 'none' }}>
-               <Button sx={buttonStyle('/my-boards')}>My Boards</Button>
-            </Link>
-            <Link to="/register" style={{ textDecoration: 'none' }}>
-               <Button sx={buttonStyle('/register')}>Register</Button>
-            </Link>
-            <Link to="/sign-in" style={{ textDecoration: 'none' }}>
-               <Button sx={buttonStyle('/login')}>Sign in</Button>
-            </Link>
-            <Link to="/sign-out" style={{ textDecoration: 'none' }}>
-               <Button sx={buttonStyle('/logout')}>Sign out</Button>
-            </Link>
-            <Link to="/about" style={{ textDecoration: 'none' }}>
-               <Button sx={buttonStyle('/about')}>About</Button>
-            </Link>
-         </Toolbar>
-      </AppBar>
+               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                  <Link to="/"
+                     style={{ textDecoration: 'none', color: theme.palette.background.paper, fontWeight: 'bold', fontStyle: 'italic' }}>
+                     Silver Feather Tavern
+                  </Link>
+               </Typography>
+               <Link to="/adventurers-board" style={{ textDecoration: 'none' }}>
+                  <Button sx={buttonStyle('/adventurers-board')}>Adventurer's Board</Button>
+               </Link>
+               <Link to="/my-boards" style={{ textDecoration: 'none' }}>
+                  <Button sx={buttonStyle('/my-boards')}>My Boards</Button>
+               </Link>
+               <Link to="/register" style={{ textDecoration: 'none' }}>
+                  <Button sx={buttonStyle('/register')}>Register</Button>
+               </Link>
+               <Link to="/sign-in" style={{ textDecoration: 'none' }}>
+                  <Button sx={buttonStyle('/sign-in')}>Sign in</Button>
+               </Link>
+               <Link to="/sign-out" style={{ textDecoration: 'none' }}>
+                  <Button sx={buttonStyle('/sign-out')}>Sign out</Button>
+               </Link>
+               <Link to="/about" style={{ textDecoration: 'none' }}>
+                  <Button sx={buttonStyle('/about')}>About</Button>
+               </Link>
+            </Toolbar>
+         </AppBar>
       </div>
    );
 }
