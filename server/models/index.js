ca1ef54ea813760316fa9ba5c175750fa5734663
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-07-31 02:46:43 +0300

    Created index file for models on server

---
+++ server/models/index.js
@@ -0,0 +1,15 @@
+const userModel = require('./userModel');
+const campaignModel = require('./campaignModel');
+const sessionModel = require('./sessionModel');
+const characterModel = require('./characterModel');
+const postModel = require('./postModel');
+const commentModel = require('./commentModel');
+
+module.exports = {
+   userModel,
+   campaignModel,
+   sessionModel,
+   characterModel,
+   postModel,
+   commentModel,
+}
