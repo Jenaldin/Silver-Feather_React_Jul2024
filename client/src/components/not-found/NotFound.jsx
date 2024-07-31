bdcd6762a5df8c52b5f2f8fc9037ac9e82eb24c8
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-07-31 23:31:44 +0300

    Edited Not Found component with content

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -1,7 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../../../public/styles/styles.css';
+import { theme } from '../../../public/styles/muiTheme';
+
 export default function NotFound() {
    return (
-      <div>
-         <h1>UNDER CONSTRUCTION</h1>
-      </div>
+      <section id="home" className='main-content'>
+         <div id="titles" className='home-titles'>
+            <h1>This path in the Tavern does not exist yet!</h1>
+            <h4>If you think it should exist, contact the administrators to create it.</h4>
+         </div>
+         <div id='img-logo'><img src="/images/not-found.png" style={{ height: '350px', width: '350px' }} ></img></div>
+
+         <h5 style={{ textAlign: 'center', margin: '10px', color: theme.palette.secondary.main }}>
+            Return to <Link to="/" className='card-links'>Home page from here</Link> or use the navigation on top.
+         </h5>
+      </section>
    )
 }
