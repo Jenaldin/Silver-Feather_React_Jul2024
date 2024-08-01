3dafaa031e761264b81c50c45553f60a0fb52903
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-08-01 14:36:49 +0300

    Edited texts

--- client/src/components/not-found/NotFound.jsx
+++ client/src/components/not-found/NotFound.jsx
@@ -7,13 +7,13 @@ export default function NotFound() {
    return (
       <section id="home" className='main-content'>
          <div id="titles" className='home-titles'>
-            <h1>This path in the Tavern does not exist yet!</h1>
-            <h4>If you think it should exist, contact the administrators to create it.</h4>
+            <h1>The path you are looking for does not exist!</h1>
+            <h4>You might have lost yourself in the labyrinth, adventurer...</h4>
          </div>
          <div id='img-logo'><img src="/images/not-found.png" style={{ height: '350px', width: '350px' }} ></img></div>
 
-         <h5 style={{ textAlign: 'center', margin: '10px', color: theme.palette.secondary.main }}>
-            Return to <Link to="/" className='card-links'>Home page from here</Link> or use the navigation on top.
+         <h5 style={{ textAlign: 'center', margin: '25px', color: theme.palette.secondary.main }}>
+            Return to <Link to="/" className='card-links'>The Entrance hall (home page)</Link> or use the signs at the top.
          </h5>
       </section>
    )
