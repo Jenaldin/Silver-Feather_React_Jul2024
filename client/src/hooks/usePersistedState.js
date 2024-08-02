166e2d16e6c216d184793c7acc4d1369852e7e9d
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-08-02 05:37:47 +0300

    Fixed persisted state

--- client/src/hooks/usePersistedState.js
+++ client/src/hooks/usePersistedState.js
@@ -4,7 +4,7 @@ export default function usePersistedState(key, initialState) {
    const [state, setState] = useState(() => {
       const persistedAuth = localStorage.getItem(key);
 
-      if(!persistedAuth){
+      if (!persistedAuth) {
          return typeof initialState === 'function' ? initialState() : initialState;
       }
 
@@ -14,7 +14,11 @@ export default function usePersistedState(key, initialState) {
 
    const updatedState = (value) => {
       const newState = typeof value === 'function' ? value(state) : value;
-      localStorage.setItem(key, JSON.stringify(newState));      
+      if (newState === null || newState === undefined) {
+         localStorage.removeItem(key);
+      } else {
+         localStorage.setItem(key, JSON.stringify(newState));
+      }
       setState(newState)
    }
 
