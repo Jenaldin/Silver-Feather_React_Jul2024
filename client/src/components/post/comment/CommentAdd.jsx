82ee60f6b0315dc97ca0e0a79be50299750843ed
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-08-06 01:30:17 +0300

    Changed content of Components that will be left for v2.0 to announce that

--- client/src/components/post/comment/CommentAdd.jsx
+++ client/src/components/post/comment/CommentAdd.jsx
@@ -1,7 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { theme } from "../common/muiTheme";
+
 export default function CommentAdd() {
    return (
-      <div>
-         <h1>UNDER CONSTRUCTION</h1>
-      </div>
-   )
+      <section id="section-wrapper" className="main-content">
+         <div id="titles" className="main-titles">
+            <h1>This "room" in the Tavern does not exist yet!</h1>
+            <h4>
+               It is a planned expansion of the Silver Feather Tavern.
+            </h4>
+         </div>
+         <div id="coming-soon">
+            <img src="/images/coming-soon.png" style={{ height: "350px", width: "350px" }} ></img>
+         </div>
+         <h6 style={{ textAlign: "center", margin: "25px", color: theme.palette.secondary.dark }} >
+            Please excuse us, but the Tavern is big and our plans are even bigger.
+            This feature will be added in version 2, as soon as our masons are
+            available.
+         </h6>
+         <h5 style={{ textAlign: "center", margin: "10px", color: theme.palette.secondary.main, }} >
+            Return to <Link to="/" className="card-links"> The Entrance hall (home page)</Link> or use the signs at the top.
+         </h5>
+      </section>
+   );
 }
