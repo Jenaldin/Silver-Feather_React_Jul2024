9ea7a420b4c5036933fdca862995d1994efab420
Jenaldin <133792511+Jenaldin@users.noreply.github.com>
2024-08-07 21:37:29 +0300

    Cleaned up comment routes

--- server/router/commentRoute.js
+++ server/router/commentRoute.js
@@ -7,9 +7,7 @@ const { isAuth } = require('../middlewares/authMiddleware');
 const { isCommentOwner } = require('../middlewares/ownerMiddleware');
 
 router.get('/:postId', commentController.getAllComments);
-router.get('/details/:commentId', commentController.commentDetails);
 router.post('/create', isAuth, commentController.createComment);
-router.put('/edit/:commentId', isCommentOwner, commentController.editComment);
 router.delete('/delete/:commentId', isCommentOwner, commentController.deleteComment);
 
 module.exports = router;
